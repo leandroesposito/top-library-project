6a34278611b41a0bd865b457ef22dec6e1c50087
Leandro Esposito <71948697+leandroesposito@users.noreply.github.com>
2025-02-10 18:59:37 -0300

    Create BookElement object

--- script.js
+++ script.js
@@ -17,3 +17,50 @@ function addBookToLibrary(author, title, numPages, read) {
     return newBook;
 }
 
+function BookElement(book) {
+    this.book = book;
+}
+
+BookElement.prototype.createElement = function (book) {
+    const newBookElement = document.createElement(div);
+    newBookElement.classList.add("card");
+    newBookElement.dataset.id = book.id;
+
+    const pAuthor = document.createElement("p");
+    const pTitle = document.createElement("p");
+    const pNumPages = document.createElement("p");
+    const pRead = document.createElement("p");
+
+    pAuthor.textContent = `Author: ${book.author}`;
+    pTitle.textContent = `Title: ${book.title}`;
+    pNumPages.textContent = `Number of pages: ${book.numPages}`;
+    pRead.textContent = `Has been read: ${book.read ? "Yes" : "No"}`;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    deleteButton.dataset.id = book.id;
+    deleteButton.addEventListener("click", handleDeleteBookClick);
+
+    newBookElement.appendChild(deleteButton);
+    newBookElement.appendChild(pTitle);
+    newBookElement.appendChild(pAuthor);
+    newBookElement.appendChild(pNumPages);
+    newBookElement.appendChild(pRead);
+
+    return newBookElement;
+};
+
+function handleDeleteBookClick(event) {
+    target = event.target;
+
+    const bookId = target.dataset.id;
+    deleteBook(bookId);
+}
+
+function deleteBook(bookId) {
+    const bookToDelete = document.querySelector(`.card[data-id="${bookId}"]`);
+
+    if (bookToDelete) {
+        bookToDelete.remove();
+    }
+}
